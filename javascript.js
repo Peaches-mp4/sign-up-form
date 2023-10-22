0bb2444cafd4dbcab96e25b45413b0db200ec2e3
Peaches-mp4 <131920771+Peaches-mp4@users.noreply.github.com>
2023-10-22 14:41:55 +0200

    Add warning messages for blank required inputs

--- javascript.js
+++ javascript.js
@@ -1,8 +1,12 @@
+// variables
+
 const pass = document.getElementById("password");
 const passConfirm = document.getElementById("password-confirm");
 const passMatch = document.getElementById("pass-match")
+const inputs = document.querySelectorAll("input");
+const required = document.getElementsByClassName('required');
 
-
+// functions for pass validation
 
 function checkPass(e) {
     passConfirm.addEventListener('blur', isSame)
@@ -10,13 +14,28 @@ function checkPass(e) {
 
 function isSame() {
    if (passConfirm.value !== pass.value) {
-    passMatch.innerText = "*Passwords don't match";
+    // passMatch.innerText = "*Passwords don't match";
+    passMatch.classList.remove('hidden');
     pass.classList.add('error');
     passConfirm.classList.add('error');
     }
 }
 
 checkPass();
+
+// functions for invalid inputs
+
+inputs.forEach(input => input.addEventListener('blur', setInvalid));
+
+function setInvalid(e) {
+    if(e.target.checkValidity() == false) {
+        e.target.classList.add('error');}
+    if(!e.target.value ) {
+        // Array.from(required).forEach(item => item.classList.remove('hidden'));
+        let precise = e.target.nextElementSibling;
+        precise.classList.remove('hidden');
+    }
+}
 
 /* 
 
